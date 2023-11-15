57a050d592a406223590e06520587a1be84a2242
Rumit Ayvovich Vokitra <sergilak280498@mail.ru>
2023-11-15 14:06:15 +0300

    v_1.0

---
+++ writeFile.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+)
+
+const (
+	TEXT_FILE_NAME = "documents/pushkin_autumn.txt"
+)
+
+func main() {
+	txt := "Унылая пора!\nОчей очерование!\nПриятна мне твоя прощальная краса -\nЛюблю я пышное природы увяданье,\nВ багрец и в золото одетые леса,\nВ их сенях ветра шум и свежее дыхание,\nИ мглой волнистою покрыты небеса, И редкий солнца луч, и первые морозы,\nИ отдалённые седой зимы угрозы."
+
+	writeTextToFile(TEXT_FILE_NAME, txt)
+
+	//textFromFile := readTextFromFile(TEXT_FILE_NAME)
+	//fmt.Println(textFromFile)
+}
+
+func writeTextToFile(filename, str string) {
+	byteToWrite := []byte(str)
+	if err := os.WriteFile(filename, byteToWrite, 0644); err != nil {
+		panic(err)
+	}
+}
